32bd3351ab350bc22b850282db3fd156a094c96e
Elberte Plínio <elberte.dev@gmail.com>
2025-10-30 22:43:53 -0300

    feat: adds `Auth Check`

--- lib/core/config/routes.dart
+++ lib/core/config/routes.dart
@@ -1,5 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:weather/features/auth/presentation/splash_screen.dart';
+import 'package:weather/features/weather/presentation/weather_screen.dart';
+
 class AppRoutes {
   static const String splash = '/';
   static const String login = '/login';
   static const String weather = '/weather';
+
+  static Widget getPageForRoute(String? route) {
+    switch (route) {
+      case weather:
+        return WeatherScreen.withProviders();
+      default:
+        return const SplashScreen();
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:weather/core/config/routes.dart';
 import 'package:weather/core/di/providers.dart';
+import 'package:weather/features/auth/presentation/auth_cubit.dart';
+import 'package:weather/features/auth/presentation/auth_state.dart';
 import 'package:weather/features/auth/presentation/login_screen.dart';
 import 'package:weather/features/auth/presentation/splash_screen.dart';
-import 'package:weather/features/weather/presentation/weather_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,12 +33,83 @@ class MyApp extends StatelessWidget {
             useMaterial3: true,
           ),
           initialRoute: AppRoutes.splash,
-          routes: {
-            AppRoutes.splash: (_) => const SplashScreen(),
-            AppRoutes.login: (_) => const LoginScreen(),
-            AppRoutes.weather: (_) => WeatherScreen.withProviders(),
+          onGenerateRoute: (settings) {
+            if (settings.name == AppRoutes.splash) {
+              return MaterialPageRoute(
+                builder: (_) => const SplashScreen(),
+                settings: settings,
+              );
+            }
+            if (settings.name == AppRoutes.login) {
+              return MaterialPageRoute(
+                builder: (_) => const LoginScreen(),
+                settings: settings,
+              );
+            }
+
+            return MaterialPageRoute(
+              builder: (context) =>
+                  _AuthCheck(child: AppRoutes.getPageForRoute(settings.name)),
+              settings: settings,
+            );
           },
         ),
+      ),
+    );
+  }
+}
+
+class _AuthCheck extends StatefulWidget {
+  const _AuthCheck({required this.child});
+
+  final Widget child;
+
+  @override
+  State<_AuthCheck> createState() => _AuthCheckState();
+}
+
+class _AuthCheckState extends State<_AuthCheck> {
+  void _checkAuthStatus() {
+    final authState = context.read<AuthCubit>().state;
+    if (authState is! AuthAuthenticated) {
+      context.read<AuthCubit>().checkAuthStatus();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _checkAuthStatus();
+  }
+
+  @override
+  void didUpdateWidget(covariant _AuthCheck oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.child != widget.child) {
+      _checkAuthStatus();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (state is AuthUnauthenticated) {
+          Navigator.of(
+            context,
+          ).pushNamedAndRemoveUntil(AppRoutes.login, (route) => false);
+        }
+      },
+      child: BlocBuilder<AuthCubit, AuthState>(
+        builder: (context, state) {
+          if (state is AuthAuthenticated) {
+            return widget.child;
+          }
+
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        },
       ),
     );
   }
